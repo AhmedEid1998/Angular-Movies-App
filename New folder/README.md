8a471516eb93e9755170c067798738ef457b7c8a
Ahmed Eid <83458048+AhmedEid1998@users.noreply.github.com>
2023-09-17 16:37:54 +0200

    edit

---
+++ New folder/README.md
@@ -0,0 +1,31 @@
+# Angular-Movies-App
+movies app with angular & api
+<br>This project was generated with <a href="https://github.com/angular/angular-cli">Angular CLI</a>.
+<br>build using Angular material.
+# Dependencies
+- Angular CLI
+- Register & Login & Logout
+- Validation Form
+- Authentication with JWT
+- Routing
+- Routing Guard
+- JWT
+- RXJS
+- Respobsive Web Design
+- Spinner
+- Owl Carousel
+- Lazy Loading
+# Technologies 
+- Angular
+- Typescript
+- Jquery
+- Javascript
+- SASS
+- Boorstrap
+- CSS
+- Html
+# Overview
+This app is presented to the user all trending Movies, Tvs and Actors and show Details of each of them
+
+# Demo
+<a href="https://ahmedeid1998.github.io/Angular-Movies-App/#/register">Movies App</a>
--- app/src/app/sevice/auth.service.ts
+++ app/src/app/sevice/auth.service.ts
@@ -5,36 +5,41 @@ import jwtDecode from 'jwt-decode';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  constructor(private _HttpClient:HttpClient, private _Router:Router) { 
-    if(localStorage.getItem('userToken') != null)
-    {
+  constructor(private _HttpClient: HttpClient, private _Router: Router) {
+    if (localStorage.getItem('userToken') != null) {
       this.setUserData();
     }
   }
-  
+
   userData = new BehaviorSubject(null);
-  register(userData:object):Observable<any>
-  {
-    return this._HttpClient.post('https://route-ecommerce-app.vercel.app/api/v1/auth/signup' , userData)
+  register(userData: object): Observable<any> {
+    // return this._HttpClient.post('https://route-ecommerce-app.vercel.app/api/v1/auth/signup' , userData)
+    return this._HttpClient.post(
+      'https://ecommerce.routemisr.com/api/v1/auth/signup',
+      userData
+    );
+    // https://ecommerce.routemisr.com/api/v1/auth/signup
   }
 
-  login(userData:object):Observable<any>
-  {
-    return this._HttpClient.post('https://route-ecommerce-app.vercel.app/api/v1/auth/signin' , userData)
-    // https://route-ecommerce-app.vercel.app/api/v1/auth/signup
+  login(userData: object): Observable<any> {
+    // return this._HttpClient.post('https://route-ecommerce-app.vercel.app/api/v1/auth/signin' , userData)
+    return this._HttpClient.post(
+      'https://ecommerce.routemisr.com/api/v1/auth/signin',
+      userData
+    );
+    // https://route-ecommerce-app.vercel.app/api/v1/auth/signin
+    // https://ecommerce.routemisr.com/api/v1/auth/signin
   }
 
-  setUserData():void
-  {
-    let endecodedToken = JSON.stringify(localStorage.getItem( 'userToken'))
-    let decodedToken:any = jwtDecode(endecodedToken);
-    this.userData.next(decodedToken) ;
+  setUserData(): void {
+    let endecodedToken = JSON.stringify(localStorage.getItem('userToken'));
+    let decodedToken: any = jwtDecode(endecodedToken);
+    this.userData.next(decodedToken);
   }
-  logout()
-  {
+  logout() {
     localStorage.removeItem('userToken');
     this.userData.next(null);
     this._Router.navigate(['/login']);
